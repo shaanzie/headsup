721d6b976a6d62226429070edde29a1666ea7ebe
shaanzie <ishaanlagwankar@gmail.com>
2020-07-05 12:10:20 +0530

    Standard login template added

---
+++ headsup/lib/login_signup.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:headsup/homepage.dart';
+
+class LandingPage extends StatelessWidget {
+  checkSignIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    // if (!checkSignIn()) {
+    //   return SignInPage();
+    // } else {
+    //   return HomePage();
+    // }
+    return HomePage();
+  }
+}
+
+class SignInPage extends StatelessWidget {
+  // _signIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Sign In'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(
+              'Login information',
+              style: TextStyle(fontSize: 20),
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(labelText: "Email Addresss"),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: "Password"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              child: Text("LOGIN"),
+              onPressed: () {},
+            )
+          ],
+        ));
+  }
+}
