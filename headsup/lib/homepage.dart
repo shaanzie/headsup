0d2f9e626fb7138e3c630f13f9992fdc31ab34c3
shaanzie <ishaanlagwankar@gmail.com>
2020-07-05 13:53:54 +0530

    Added localstorage classes

--- headsup/lib/homepage.dart
+++ headsup/lib/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:headsup/login_signup.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,12 +9,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  _logout() {
+  _logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
     return Navigator.pushReplacement(
         context,
         MaterialPageRoute(
           builder: (context) => SignInPage(),
         ));
+  }
+
+  String _user;
+  @override
+  void initState() {
+    super.initState();
+    _getUser();
+  }
+
+  void _getUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _user = prefs.getString('user');
+    print(prefs.getString('user'));
   }
 
   @override
@@ -22,14 +39,17 @@ class _HomePageState extends State<HomePage> {
         title: Text("HomePage"),
         actions: <Widget>[
           RaisedButton(
-            child: Text("LOGOUT"),
-            color: Colors.transparent,
+            child: Text(
+              "LOGOUT",
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.indigo,
             onPressed: _logout,
           )
         ],
       ),
       body: Center(
-        child: Text('Home Page Content'),
+        child: Text("Hello, $_user!"),
       ),
     );
   }
