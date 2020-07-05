c186f98aeab5534d97d66d8b718e51101cc21df2
shaanzie <ishaanlagwankar@gmail.com>
2020-07-05 14:58:20 +0530

    Added login functions

--- headsup/lib/login_signup.dart
+++ headsup/lib/login_signup.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:headsup/dashboard_manager.dart';
 import 'package:headsup/dashboard_worker.dart';
-
-import 'dashboard_worker.dart';
+import 'package:http/http.dart' as http;
 
 class LandingPage extends StatelessWidget {
   checkSignIn() {}
@@ -18,8 +17,26 @@ class LandingPage extends StatelessWidget {
   }
 }
 
-class SignInPage extends StatelessWidget {
-  // _signIn() {}
+class Credentials {
+  final String email;
+  final String password;
+
+  Credentials({this.email, this.password});
+
+  factory Credentials.fromJson(Map<String, dynamic> json) {
+    return Credentials(email: json['email'], password: json['password']);
+  }
+}
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _password;
+  String _email;
 
   @override
   Widget build(BuildContext context) {
@@ -27,37 +44,45 @@ class SignInPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Sign In'),
         ),
-        body: Column(
-          children: <Widget>[
-            Text(
-              'Login information',
-              style: TextStyle(fontSize: 20),
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(labelText: "Email Addresss"),
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: "Password"),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            RaisedButton(
-              child: Text("LOGIN"),
-              onPressed: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      //if worker
-                      builder: (context) => Dashboard(),
-                      //else
-                      //builder: (context) => Dashboard2(),
-                    ));
-              },
-            )
-          ],
-        ));
+        body: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Login information',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    TextFormField(
+                      onSaved: (newValue) => _email = newValue,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(labelText: "Email Addresss"),
+                    ),
+                    TextFormField(
+                      onSaved: (newValue) => _password = newValue,
+                      obscureText: true,
+                      decoration: InputDecoration(labelText: "Password"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    RaisedButton(
+                      child: Text("LOGIN"),
+                      onPressed: () {
+                        final form = _formKey.currentState;
+                        form.save();
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              //if worker
+                              builder: (context) => Dashboard(),
+                              //else
+                              //builder: (context) => Dashboard2(),
+                            ));
+                      },
+                    )
+                  ],
+                ))));
   }
 }
