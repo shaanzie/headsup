1d54df3bd5930c7e1e6c794217402e4d369699ea
Taran <42052166+Wiking-24@users.noreply.github.com>
2020-07-06 22:50:53 +0530

    Completed manager ui structure

--- headsup/lib/ManagerUI/main.dart
+++ headsup/lib/ManagerUI/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:headsup/ManagerUI/bar_chart.dart';
-import 'package:headsup/ManagerUI/pie_chart.dart';
 import 'package:headsup/ManagerUI/line_chart.dart';
-
+import 'package:headsup/login_signup.dart';
 //import 'package:shared_preferences/shared_preferences.dart';
+import 'package:headsup/ManagerUI/pie_chart.dart';
+import 'package:headsup/ManagerUI/productivity.dart';
+import 'package:headsup/ManagerUI/fatigue.dart';
 
 void main() => runApp(MyApp());
 
@@ -191,64 +193,182 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Analytics Dashboard"),
-        actions: <Widget>[
-          RaisedButton(
-            child: Text(
-              "LOGOUT",
-              style: TextStyle(color: Colors.white),
-            ),
-            color: Colors.cyan,
-            onPressed: _logout,
-          )
-        ],
-      ),
-      body: Center(
-        child: SizedBox(
-          height: 600, // card height
-          child: PageView.builder(
-            itemCount: 3,
-            controller: PageController(viewportFraction: 0.7),
-            onPageChanged: (int index) => setState(() => _index = index),
-            itemBuilder: (_, i) {
-              return Transform.scale(
-                scale: i == _index ? 1 : 0.9,
-                child: Card(
-                  elevation: 6,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Center(
-                      child: InkWell(
-                    onTap: () {
-                      if (i == 0) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => BarChartSample2()));
-                      } else if (i == 1) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LineChartSample1()));
-                      } else if (i == 2) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => PieChartSample2()));
-                      }
-                    },
+        appBar: AppBar(
+          title: Text("Analytics Dashboard"),
+          actions: <Widget>[
+            RaisedButton(
+              child: Text(
+                "LOGOUT",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.cyan,
+              onPressed: _logout,
+            )
+          ],
+        ),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Expanded(
                     child: Text(
-                      "Card ${i + 1}",
-                      style: TextStyle(fontSize: 32),
+                      "Welcome back, '\$_user'",
+                      style: TextStyle(
+                        fontSize: 20,
+                        height: 5,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   )),
+              Center(
+                child: SizedBox(
+                  height: 500, // card height
+                  child: PageView.builder(
+                    itemCount: 3,
+                    controller: PageController(viewportFraction: 0.7),
+                    onPageChanged: (int index) =>
+                        setState(() => _index = index),
+                    itemBuilder: (_, i) {
+                      if (i == 0) {
+                        return Transform.scale(
+                            scale: i == _index ? 1 : 0.9,
+                            child: GestureDetector(
+                              child: Card(
+                                elevation: 6,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                child: Stack(
+                                  children: <Widget>[
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          image: DecorationImage(
+                                              fit: BoxFit.fill,
+                                              colorFilter: new ColorFilter.mode(
+                                                  Colors.black.withOpacity(0.7),
+                                                  BlendMode.dstATop),
+                                              image: AssetImage(
+                                                  "assets/images/productivity.png"))),
+                                    ),
+                                    Container(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        "Productivity",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 29),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          Productivity(), // Change to Cal Count
+                                    ));
+                              },
+                            ));
+                      } else if (i == 1) {
+                        return Transform.scale(
+                            scale: i == _index ? 1 : 0.9,
+                            child: GestureDetector(
+                              child: Card(
+                                elevation: 6,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                child: Stack(
+                                  children: <Widget>[
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          image: DecorationImage(
+                                              fit: BoxFit.fill,
+                                              colorFilter: new ColorFilter.mode(
+                                                  Colors.black.withOpacity(0.7),
+                                                  BlendMode.dstATop),
+                                              image: AssetImage(
+                                                  "assets/images/worker.jpg"))),
+                                    ),
+                                    Container(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        "Worker Count",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 29),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          PieChartSample2(), // Change to Cal Count
+                                    ));
+                              },
+                            ));
+                      } else {
+                        return Transform.scale(
+                            scale: i == _index ? 1 : 0.9,
+                            child: GestureDetector(
+                              child: Card(
+                                elevation: 6,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                child: Stack(
+                                  children: <Widget>[
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          image: DecorationImage(
+                                              fit: BoxFit.fill,
+                                              colorFilter: new ColorFilter.mode(
+                                                  Colors.black.withOpacity(0.7),
+                                                  BlendMode.dstATop),
+                                              image: AssetImage(
+                                                  "assets/images/fatigue.jpg"))),
+                                    ),
+                                    Container(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        "Fatigue",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 29),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          Fatigue(), // Change to Cal Count
+                                    ));
+                              },
+                            ));
+                      }
+                    },
+                  ),
                 ),
-              );
-            },
+              ),
+            ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
--- headsup/lib/login_signup.dart
+++ headsup/lib/login_signup.dart
@@ -91,7 +91,7 @@ class _SignInPageState extends State<SignInPage> {
                             context,
                             MaterialPageRoute(
                                 //if worker
-                                builder: (context) => WorkerDashboard()));
+                                builder: (context) => MyApp()));
                         // }
                       },
                     )
