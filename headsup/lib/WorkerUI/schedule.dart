b54df57d26b48667a2a7ad55b301aa828fac155c
shaanzie <ishaanlagwankar@gmail.com>
2020-07-06 14:18:41 +0530

    Added scaffolds on schedule

--- headsup/lib/WorkerUI/schedule.dart
+++ headsup/lib/WorkerUI/schedule.dart
@@ -14,50 +14,54 @@ class ScheduleActivity extends StatelessWidget {
     DateTime date = DateTime(now.year, now.month, now.day);
 
 // And here's our widget !
-    return WeekView(
-      style: WeekViewStyle(
-        dayBarHeight: 200,
-        hourRowHeight: 40,
-        dayViewWidth: 100,
-        hoursColumnTextStyle: TextStyle(fontSize: 10, color: Colors.black87),
+    return Scaffold(
+      appBar: AppBar(title: Text("Schedule")),
+      body: WeekView(
+        style: WeekViewStyle(
+          dayBarHeight: 50,
+          hourRowHeight: 40,
+          hoursColumnTextStyle: TextStyle(fontSize: 10, color: Colors.black87),
+        ),
+        scrollToCurrentTime: true,
+        userZoomable: true,
+        dates: [
+          date.subtract(Duration(days: 1)),
+          date,
+          date.add(Duration(days: 1))
+        ],
+        events: [
+          FlutterWeekViewEvent(
+            title: 'An event 1',
+            description: 'A description 1',
+            start: date.subtract(Duration(hours: 1)),
+            end: date.add(Duration(hours: 18, minutes: 30)),
+          ),
+          FlutterWeekViewEvent(
+            title: 'An event 2',
+            description: 'A description 2',
+            start: date.add(Duration(hours: 19)),
+            end: date.add(Duration(hours: 22)),
+          ),
+          FlutterWeekViewEvent(
+            title: 'An event 3',
+            description: 'A description 3',
+            start: date.add(Duration(hours: 23, minutes: 30)),
+            end: date.add(Duration(hours: 25, minutes: 30)),
+          ),
+          FlutterWeekViewEvent(
+            title: 'An event 4',
+            description: 'A description 4',
+            start: date.add(Duration(hours: 20)),
+            end: date.add(Duration(hours: 21)),
+          ),
+          FlutterWeekViewEvent(
+            title: 'An event 5',
+            description: 'A description 5',
+            start: date.add(Duration(hours: 20)),
+            end: date.add(Duration(hours: 21)),
+          ),
+        ],
       ),
-      dates: [
-        date.subtract(Duration(days: 1)),
-        date,
-        date.add(Duration(days: 1))
-      ],
-      events: [
-        FlutterWeekViewEvent(
-          title: 'An event 1',
-          description: 'A description 1',
-          start: date.subtract(Duration(hours: 1)),
-          end: date.add(Duration(hours: 18, minutes: 30)),
-        ),
-        FlutterWeekViewEvent(
-          title: 'An event 2',
-          description: 'A description 2',
-          start: date.add(Duration(hours: 19)),
-          end: date.add(Duration(hours: 22)),
-        ),
-        FlutterWeekViewEvent(
-          title: 'An event 3',
-          description: 'A description 3',
-          start: date.add(Duration(hours: 23, minutes: 30)),
-          end: date.add(Duration(hours: 25, minutes: 30)),
-        ),
-        FlutterWeekViewEvent(
-          title: 'An event 4',
-          description: 'A description 4',
-          start: date.add(Duration(hours: 20)),
-          end: date.add(Duration(hours: 21)),
-        ),
-        FlutterWeekViewEvent(
-          title: 'An event 5',
-          description: 'A description 5',
-          start: date.add(Duration(hours: 20)),
-          end: date.add(Duration(hours: 21)),
-        ),
-      ],
     );
   }
 }
