15e48fd74c33dad79b65b41112729bc7817def52
Taran <42052166+Wiking-24@users.noreply.github.com>
2020-07-06 23:37:24 +0530

    Finished manager

--- headsup/lib/ManagerUI/main.dart
+++ headsup/lib/ManagerUI/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:headsup/ManagerUI/bar_chart.dart';
 import 'package:headsup/ManagerUI/line_chart.dart';
 import 'package:headsup/login_signup.dart';
-//import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:headsup/ManagerUI/pie_chart.dart';
 import 'package:headsup/ManagerUI/productivity.dart';
 import 'package:headsup/ManagerUI/fatigue.dart';
@@ -33,162 +33,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  /*_logout() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.clear();
-    return Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => SignInPage(),
-        ));
-  }*/
-  _logout() async {}
-/*
+  int touchedIndex = 0;
   String _user;
+  int _index = 0;
+
+  _getUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _user = prefs.get('email');
+    print("User: " + _user);
+  }
+
   @override
   void initState() {
     super.initState();
     _getUser();
   }
-
-  void _getUser() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    _user = prefs.getString('email');
-    print(_user);
-  }*/
-  /*
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Carousel in vertical scrollable'),
-      ),
-      body: ListView.builder(
-        padding: EdgeInsets.symmetric(vertical: 16.0),
-        itemBuilder: (BuildContext context, int index) {
-          if (index % 2 == 0) {
-            return _buildCarousel(context, index ~/ 2);
-          } else {
-            return Divider();
-          }
-        },
-      ),
-    );
-  }
-
-  Widget _buildCarousel(BuildContext context, int carouselIndex) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Text('Carousel $carouselIndex'),
-        SizedBox(
-          // you may want to use an aspect ratio here for tablet support
-          height: 200.0,
-          child: PageView.builder(
-            // store this controller in a State to save the carousel scroll position
-            controller: PageController(viewportFraction: 0.8),
-            itemBuilder: (BuildContext context, int itemIndex) {
-              return _buildCarouselItem(context, carouselIndex, itemIndex);
-            },
-          ),
-        )
-      ],
-    );
-  }
-
-  Widget _buildCarouselItem(
-      BuildContext context, int carouselIndex, int itemIndex) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 4.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.all(Radius.circular(4.0)),
-        ),
-      ),
-    );
-  }
-  
-  Widget _buildCard(String assetUrl, String title, int index) {
-    final double deviceHeight = MediaQuery.of(context).size.height;
-    return SizedBox(
-      width: deviceHeight * 0.45,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20.0),
-        child: InkWell(
-          onTap: () {
-            if (index == 1) {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => BarChart()));
-            } else if (index == 2) {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => LineChart()));
-            } else if (index == 3) {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => PieChart()));
-            }
-          },
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: deviceHeight * 0.3,
-                decoration: BoxDecoration(
-                    color: Colors.black,
-                    image: DecorationImage(
-                        fit: BoxFit.fill,
-                        colorFilter: new ColorFilter.mode(
-                            Colors.black.withOpacity(0.7), BlendMode.dstATop),
-                        image: AssetImage(assetUrl))),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: deviceHeight * 0.125),
-                alignment: Alignment.center,
-                child: Text(
-                  title,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 29),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              height: 25,
-            ),
-            _buildCard('assets/images/barchart.jpg', 'Bar-Chart Example', 1),
-            SizedBox(
-              height: 25,
-            ),
-            _buildCard('assets/images/linechart.png', 'Line-Chart Example', 2),
-            SizedBox(
-              height: 25,
-            ),
-            _buildCard('assets/images/piechart.jpg', 'Pie-Chart Example', 3)
-          ],
-        ),
-      ),
-    );
-  }*/
-
-  int _index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -202,7 +61,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: TextStyle(color: Colors.white),
               ),
               color: Colors.cyan,
-              onPressed: _logout,
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                prefs.clear();
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => SignInPage()));
+              },
             )
           ],
         ),
