d3d97c1e2b8c2e892bc696b7b943d68756162d7c
shaanzie <ishaanlagwankar@gmail.com>
2020-07-07 23:12:00 +0530

    Fixed UI

--- headsup/lib/main.dart
+++ headsup/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: SignInPage(),
+      home: LandingPage(),
       debugShowCheckedModeBanner: false,
     );
   }
