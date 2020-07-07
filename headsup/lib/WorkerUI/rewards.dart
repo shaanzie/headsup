45ec94479ee40697c306e9dfb76437d1c1256c8c
shaanzie <ishaanlagwankar@gmail.com>
2020-07-07 11:43:57 +0530

    Sample rewards page done

--- headsup/lib/WorkerUI/rewards.dart
+++ headsup/lib/WorkerUI/rewards.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -15,7 +16,149 @@ class RewardActivity extends StatefulWidget {
 
 class _RewardActivityState extends State<RewardActivity> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Rewards"),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              height: 220,
+              width: double.maxFinite,
+              child: Card(
+                elevation: 5,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset('assets/phone.png'),
+                    ),
+                    Center(
+                      child: Column(
+                        children: <Widget>[
+                          Center(
+                            child: Text(
+                              "Reward 1",
+                              style: GoogleFonts.corben(fontSize: 30),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              "Points: 200",
+                              style: GoogleFonts.corben(fontSize: 20),
+                            ),
+                          ),
+                          Center(
+                            child: FlatButton(
+                              child: Text(
+                                "AVAIL",
+                                style: GoogleFonts.aladin(fontSize: 20),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              height: 220,
+              width: double.maxFinite,
+              child: Card(
+                elevation: 5,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset('assets/phone.png'),
+                    ),
+                    Center(
+                      child: Column(
+                        children: <Widget>[
+                          Center(
+                            child: Text(
+                              "Reward 2",
+                              style: GoogleFonts.corben(fontSize: 30),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              "Points: 400",
+                              style: GoogleFonts.corben(fontSize: 20),
+                            ),
+                          ),
+                          Center(
+                            child: FlatButton(
+                              child: Text(
+                                "AVAIL",
+                                style: GoogleFonts.aladin(fontSize: 20),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              height: 220,
+              width: double.maxFinite,
+              child: Card(
+                elevation: 5,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset('assets/phone.png'),
+                    ),
+                    Center(
+                      child: Column(
+                        children: <Widget>[
+                          Center(
+                            child: Text(
+                              "Reward 3",
+                              style: GoogleFonts.corben(fontSize: 30),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              "Points: 500",
+                              style: GoogleFonts.corben(fontSize: 20),
+                            ),
+                          ),
+                          Center(
+                            child: FlatButton(
+                              child: Text(
+                                "AVAIL",
+                                style: GoogleFonts.aladin(fontSize: 20),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
