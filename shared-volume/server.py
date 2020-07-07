318a018995cc16c1f8fda704b0fa6b8301ff0604
drholmie <animesh.leo@gmail.com>
2020-07-07 11:32:02 +0530

    Added examples to server.py

--- shared-volume/server.py
+++ shared-volume/server.py
@@ -2,7 +2,7 @@
 #curl -d '{"email":"value1@gmail.com", "password":"value2", "name":"value1","role":"supervisor"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/signup
 #curl -d '{"timestamp":"12345","stepcount":"200","duration":"1","employeeID":"1234","type":"productivity","asleep":"20","active":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
 #curl -d '{"type":"person","employeeID":"1234"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pulldata
-#
+#curl -d '{"type":"collective"} -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pulldata
 import os
 import json
 import uuid
