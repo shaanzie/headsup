05a0e4f59635dd532a0522e150c20db84091929b
drholmie <animesh.leo@gmail.com>
2020-07-07 20:48:13 +0530

    Fully updated APIs and dbsetup script

--- shared-volume/server.py
+++ shared-volume/server.py
@@ -3,6 +3,7 @@
 #curl -d '{"timestamp":"12345","stepcount":"200","duration":"1","employeeID":"1234","type":"productivity","asleep":"20","active":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
 #curl -d '{"type":"person","employeeID":"1234"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pulldata
 #curl -d '{"type":"collective"} -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pulldata
+#1594133266
 import os
 import json
 import uuid
@@ -10,6 +11,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from pymongo import MongoClient
 import sys
+import random
 app = Flask(__name__)
 CORS(app)
 client = MongoClient(
@@ -43,18 +45,70 @@ def login():
     else:
         return {"message":"Wrong username or password!"}, 403
 
+@app.route('/api/v1/fatigue', methods=['POST'])
+def fatigue():
+    data = request.get_json()
+    arr = []
+    sum_arr=[0,0,0]
+    _items = db.fatigue.find({'employeeID':data['employeeID']})
+    for item in _items:
+        arr.append(item['alpha'])
+        sum_arr[0]+=float(item['alpha'])
+        sum_arr[1]+=float(item['beta'])
+        sum_arr[2]+=float(item['gamma'])
+    print(arr,file=sys.stderr)
+    print(sum_arr,file=sys.stderr)
+    return {'message':'success', "data":arr, "sum":sum_arr}, 201
+
+@app.route('/api/v1/pollution', methods=['POST'])
+def pollution():
+    data = request.get_json()
+    arr = []
+    _items = db.pollution.find({'employeeID':data['employeeID']})
+    for item in _items:
+        arr.append(item['pollution'])
+    val = random.choices(arr)
+    return {'message':'success', 'data':val[0]}, 201
+
+@app.route('/api/v1/stepcount', methods=['POST'])
+def stepcount():
+    data = request.get_json()
+    _items = db.productivity.find({'employeeID':data['employeeID']})
+    steps = 0
+    for item in _items:
+        steps += int(item['stepcount'])
+    return {'message':'success', 'data':steps}, 201
+
+@app.route('/api/v1/workercount', methods=['GET'])
+def workercount():
+    arr = []
+    _items = db.productivity.find({"status":"active"})
+    count = 0
+    for item in _items:
+        timestamp = int(item['timestamp'])
+        count = db.productivity.count_documents({'status':"active","timestamp":{"$gte":str(timestamp), "$lte": str(timestamp+3600)}})
+        arr.append(count)
+        print(arr,file=sys.stderr)
+        print(timestamp+3600,file=sys.stderr)
+        if db.productivity.count_documents({"status":"active", "timestamp":{"$gt":str(timestamp+3600)}}):
+            _items = db.productivity.find({"status":"active", "timestamp":{"$gt":str(timestamp+3600)}})
+        else:
+            break
+    return {'message':'success','data':arr}, 200
+
 @app.route('/api/v1/pushdata', methods=['POST'])
 def pushdata():
     data = request.get_json()
     print(data, file=sys.stderr)
     if data['type'] == "productivity":
         db.productivity.insert_one({'timestamp':data['timestamp'],'duration':data['duration'],'employeeID': data['employeeID'],
-        'asleep': data['asleep'],'active': data['active'],'stepcount': data['stepcount']})
+        'asleep': data['asleep'],'active': data['active'],'stepcount': data['stepcount'], 'status': data['status']})
         return {"message":"Success"}, 201
-    elif data['type'] == "status":
-        pass
     elif data['type'] == "pollution":
         db.pollution.insert_one({'employeeID': data['employeeID'], 'pollution': data['pollution'] })
+        return {"message":"Success"}, 201
+    elif data['type'] == "fatigue":
+        db.fatigue.insert_one({'employeeID':data['employeeID'], 'alpha':data['alpha'], 'beta':data['beta'], 'gamma': data['gamma']})
         return {"message":"Success"}, 201
     return {"message":"Failed"}, 403
 
@@ -79,7 +133,7 @@ def pulldata():
                 sumact_item += float(peritem['active'])
                 sumasl_item += float(peritem['asleep'])
                 sumcount_item += float(peritem['stepcount'])
-            sum_list[item['employeeID']].append(sumcount_item + sumact_item/60*sumdur_item)
+            sum_list[item['employeeID']].append(sumcount_item/25000 + sumact_item/60*sumdur_item)
             sum_list = {k: v for k, v in sorted(sum_list.items(), key=lambda item: item[1])}
             ord_arr=[item for item in sum_list.keys()]
             ord_arr.reverse()
@@ -95,7 +149,7 @@ def pulldata():
             sumasl_item = float(item['asleep'])
             sumcount_item = float(item['stepcount'])
             print(item, file=sys.stderr)
-            sum_list.append(sumcount_item + sumact_item/60*sumdur_item) #{'stepcount':sumcount_item,'active':sumact_item, 'asleep': sumasl_item, 'employeeID': data['employeeID']}
+            sum_list.append(sumcount_item/25000 + sumact_item/60*sumdur_item) #{'stepcount':sumcount_item,'active':sumact_item, 'asleep': sumasl_item, 'employeeID': data['employeeID']}
         return {'message':'success', 'data':sum_list}, 201
     return 201
 
--- shared-volume/setupdb.sh
+++ shared-volume/setupdb.sh
@@ -1,14 +1,72 @@
-#signup
+dates=$(date -u +%s)
+echo $dates
+set -e
+
+echo "signup"
 curl -d '{"email":"value1@gmail.com", "password":"value2", "name":"value1","role":"supervisor", "employeeID":"1234"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/signup
-curl -d '{"email":"value2@gmail.com", "password":"value1", "name":"value1","role":"worker", "employeeID":"12345"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/signup
+curl -d '{"email":"value2@gmail.com", "password":"value1", "name":"value2","role":"worker", "employeeID":"234"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/signup
+curl -d '{"email":"value3@gmail.com", "password":"value4", "name":"value3","role":"worker", "employeeID":"567"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/signup
+curl -d '{"email":"value4@gmail.com", "password":"value3", "name":"value4","role":"worker", "employeeID":"890"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/signup
+echo " "
+echo " "
+echo "pushdata"
+curl -d '{"status":"active","timestamp":"'${dates}'","stepcount":"4000","duration":"1","employeeID":"567","type":"productivity","asleep":"20","active":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"active","timestamp":"'$((${dates}+3600))'","stepcount":"2000","duration":"1","employeeID":"567","type":"productivity","asleep":"10","active":"50"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"active","timestamp":"'$((${dates}+3600))'","stepcount":"1900","duration":"1","employeeID":"567","type":"productivity","asleep":"20","active":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"active","timestamp":"'$((${dates}+7200))'","stepcount":"2000","duration":"1","employeeID":"567","type":"productivity","asleep":"0","active":"0"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
 
-#pushdata
-curl -d '{"timestamp":"12345","stepcount":"200","duration":"1","employeeID":"1234","type":"productivity","asleep":"20","active":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
-curl -d '{"timestamp":"12356","stepcount":"200","duration":"1","employeeID":"1234","type":"productivity","asleep":"10","active":"50"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"active","timestamp":"'${dates}'","stepcount":"3000","duration":"1","employeeID":"234","type":"productivity","asleep":"20","active":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"active","timestamp":"'$((${dates}+3600))'","stepcount":"2100","duration":"1","employeeID":"234","type":"productivity","asleep":"10","active":"50"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"active","timestamp":"'$((${dates}+3600))'","stepcount":"1000","duration":"1","employeeID":"234","type":"productivity","asleep":"10","active":"50"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"passive","timestamp":"'$((${dates}+7200))'","stepcount":"0","duration":"1","employeeID":"234","type":"productivity","asleep":"0","active":"0"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
 
-curl -d '{"timestamp":"12345","stepcount":"200","duration":"1","employeeID":"12345","type":"productivity","asleep":"20","active":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
-curl -d '{"timestamp":"12356","stepcount":"200","duration":"1","employeeID":"12345","type":"productivity","asleep":"10","active":"50"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"active","timestamp":"'${dates}'","stepcount":"3100","duration":"1","employeeID":"890","type":"productivity","asleep":"20","active":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"active","timestamp":"'$((${dates}+3600))'","stepcount":"2000","duration":"1","employeeID":"890","type":"productivity","asleep":"10","active":"50"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"active","timestamp":"'$((${dates}+3600))'","stepcount":"2100","duration":"1","employeeID":"890","type":"productivity","asleep":"10","active":"50"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"status":"passive","timestamp":"'$((${dates}+7200))'","stepcount":"0","duration":"1","employeeID":"890","type":"productivity","asleep":"0","active":"0"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
 
-#pull examples
-curl -d '{"type":"person","employeeID":"1234"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pulldata
+curl -d '{"employeeID":"567","type":"fatigue","alpha":"20","beta":"40", "gamma":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"567","type":"fatigue","alpha":"10","beta":"50", "gamma":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"567","type":"fatigue","alpha":"20","beta":"40", "gamma":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"567","type":"fatigue","alpha":"30","beta":"20", "gamma":"50"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+
+curl -d '{"employeeID":"234","type":"fatigue","alpha":"20","beta":"40", "gamma":"40"}'  -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"234","type":"fatigue","alpha":"10","beta":"50", "gamma":"40"}'  -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"234","type":"fatigue","alpha":"20","beta":"40", "gamma":"40"}'  -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"234","type":"fatigue","alpha":"30","beta":"20", "gamma":"50"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+
+curl -d '{"employeeID":"890","type":"fatigue","alpha":"20","beta":"40", "gamma":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"890","type":"fatigue","alpha":"10","beta":"50", "gamma":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"890","type":"fatigue","alpha":"20","beta":"40", "gamma":"40"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"890","type":"fatigue","alpha":"30","beta":"20", "gamma":"50"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+
+curl -d '{"employeeID":"234","type":"pollution","pollution":"8"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"234","type":"pollution","pollution":"9.6"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"234","type":"pollution","pollution":"7"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+curl -d '{"employeeID":"234","type":"pollution","pollution":"6.4"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pushdata
+
+echo ""
+echo ""
+echo "pull examples"
+curl -d '{"type":"person","employeeID":"234"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pulldata
 curl -d '{"type":"collective"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pulldata
+
+echo " "
+echo " "
+echo "fatigue"
+curl -d '{"employeeID":"234"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/fatigue
+
+echo " "
+echo " "
+echo "stepcount"
+curl -d '{"employeeID":"234"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/stepcount
+
+echo " "
+echo " "
+echo "pollution"
+curl -d '{"employeeID":"234"}' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/pollution
+
+echo " "
+echo " "
+echo "workercount"
+curl http://localhost:5000/api/v1/workercount
